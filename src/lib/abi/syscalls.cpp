7f2123cb18ab618ad2334dbdbe9ec7b8d335a303
Abb1x <abbix@riseup.net>
2021-11-22 18:59:25 -0500

    kernel: stop spamming extern C

--- src/lib/abi/syscalls.cpp
+++ src/lib/abi/syscalls.cpp
@@ -1,39 +1,39 @@
 #include "abi/syscalls.hpp"
 #include <syscalls.hpp>
 
-extern "C" TxResult tx_sys_debug(const char *args)
+extern "C"
 {
-    return tx_syscall(TX_SYS_DEBUG, (uint64_t)args);
-}
+    TxResult tx_sys_debug(const char *args)
+    {
+        return tx_syscall(TX_SYS_DEBUG, (uint64_t)args);
+    }
 
-extern "C" TxResult tx_sys_ipc(TxIpc *ipc)
-{
-    return tx_syscall(TX_SYS_IPC, (uint64_t)ipc);
-}
+    TxResult tx_sys_ipc(TxIpc *ipc)
+    {
+        return tx_syscall(TX_SYS_IPC, (uint64_t)ipc);
+    }
 
-extern "C" TxResult tx_sys_bind(TxEvent *event)
-{
-    return tx_syscall(TX_SYS_BIND, (uint64_t)event);
-}
+    TxResult tx_sys_bind(TxEvent *event)
+    {
+        return tx_syscall(TX_SYS_BIND, (uint64_t)event);
+    }
 
-extern "C" TxResult tx_sys_map(TxMap *map)
-{
-    return tx_syscall(TX_SYS_MAP, (uint64_t)map);
-}
+    TxResult tx_sys_map(TxMap *map)
+    {
+        return tx_syscall(TX_SYS_MAP, (uint64_t)map);
+    }
 
-extern "C" TxResult tx_syscall(TxSyscall syscall, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4)
-{
-    uint64_t res = 0;
+    TxResult tx_syscall(TxSyscall syscall, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4)
+    {
+        uint64_t res = 0;
 #ifdef __x86_64__
 
-    register uint64_t r8 asm("r8") = arg3;
-    register uint64_t r9 asm("r9") = arg4;
-
-    asm volatile("int $0x42"
-                 : "=a"(res)
-                 : "a"(syscall), "b"(arg0), "c"(arg1), "d"(arg2), "r"(r8), "r"(r9)
-                 : "memory");
+        asm volatile("int $0x42"
+                     : "=a"(res)
+                     : "a"(syscall), "b"(arg0), "c"(arg1), "d"(arg2), "S"(arg3), "D"(arg4)
+                     : "memory");
 #endif
 
-    return res;
+        return res;
+    }
 }
